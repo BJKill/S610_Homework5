b110cef8686fd51c005f7be894966eefa90f7c54
BJKill <brandon.j.kill@gmail.com>
2020-10-19 15:16:08 -0400

    Corrected test_llr.R

--- test_llr.R
+++ test_llr.R
@@ -4,12 +4,12 @@ install.packages("testthat")
 library(testthat)
 
 test_that("llr output has correct length", {
-  expect_equal(length(llr(x, y, z, omega = 1), length(z)))
+  expect_equal(length(llr(x, y, z, omega = 1)), length(z)))
 })
 
 test_that("make_weight_matrix works on simple cases", {
   Wz <- make_weight_matrix(z[1],x,1)
-  expect_true(all(Wz[upper.tri(Wz)], Wz[lower.tri(Wz)] == 0))
+  expect_true(all(Wz[upper.tri(Wz)] == 0, Wz[lower.tri(Wz)] == 0))
   r <- abs(x-z[1])/1
   expect_equal(diag(Wz), sapply(r,W))
 })
